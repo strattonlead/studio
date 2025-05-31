da33750a62625a9c561f86d2a08c73488d3a21ca
Arndt Bieberstein <arndt.bieberstein@gmail.com>
2025-05-31 09:44:18 +0000

    tmp

--- src/components/booking-iframe.tsx
+++ src/components/booking-iframe.tsx
@@ -5,13 +5,12 @@ type BookingIframeProps = {
 };
 
 export function BookingIframe({ 
-  src = "https://widget.getinminthair.com/shops/641a69a3-79d2-4277-a04a-3e4616502c67", 
+  src = "https://salonkee.de/salon/hairskin-couture/booking", 
   title = "Online Terminbuchung Hair & Skin Couture",
   className 
 }: BookingIframeProps) {
   return (
     <div className={`aspect-[3/4] md:aspect-video max-w-4xl mx-auto w-full rounded-lg overflow-hidden shadow-xl border border-border ${className}`}>
-      <img src="/termin-platzhalter.png"></img>
       <iframe
         src={src}
         title={title}
